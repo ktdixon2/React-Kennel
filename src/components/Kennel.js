1a31853500b7b7edf1211bca66db3bdfe7a22674
Kenneth Dixon <kensmacbookpro@Kenneths-MacBook-Pro.local>
2019-05-17 15:55:11 -0500

    added location compenent

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+import LocationList from "./Locations.js"
 
 export default class Kennel extends Component {
     render() {
         return (
-            <div>
+            <div id="kennelContainer">
                 <h3>Student Kennels</h3>
                 <h4>Nashville North Location</h4>
                 <h5>500 Puppy Way</h5>
                 <EmployeeList />
+                <LocationList />
             </div>
         );
     }
---
+++ src/components/Locations.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class LocationList extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>Nashville, TN</section>
+                <section>Knoxville, TN</section>
+                <section>Memphis, TN</section>
+            </article>
+        );
+    }
+}
